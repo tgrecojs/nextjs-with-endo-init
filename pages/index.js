099e6364068d73009e40459d7705e7d98a5fd4d1
Thomas Greco <tgrecojs@fastmail.com>
2024-02-21 02:38:25 -0500

    chore: removed logs

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import { isServer } from "../utils.js";
 
-const Index = () => {
-  //
-  console.log(isServer(globalThis));
-  return (
-    <div>
-      <h2>Next.js Application - 21 February 2024</h2>
-      <p></p>
-      {/* <p>{`${isServer(globalThis)}`}</p> */}
-    </div>
-  );
-};
+const Index = () => (
+  <div>
+    <h2>Next.js Application - 21 February 2024</h2>
+    <p></p>
+    {/* <p>{`${isServer(globalThis)}`}</p> */}
+  </div>
+);
 
 export default Index;
